130f9c8ace3bcc7a53da90e4059214854ca3c8e5
Juan Antonio Sancho <juan.sancho.hidalgo.al@iespoligonosur.org>
2025-03-27 13:25:45 +0100

    Saludo con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,8 @@ public class HolaMundo{
 	public static void main(String[]args){
 	String naranja = "\033[33m";
 	String azul = "\033[34m";
-	System.out.println("¡Hola Mundo!);
-	System.out.println("Que pasa! Como estas!");
+	
+	System.out.println(naranja + "¡Hola Mundo!");
+	System.out.println(azul + "Que pasa! Como estas!");
 	}
 }
